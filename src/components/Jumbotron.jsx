0b299e980de1b3da4354b77a58fa007753dd33f6
ViktoriiaYakaba <yakaba@bk.ru>
2024-05-27 19:45:08 +0200

    add soundSection.jsx

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,15 @@ import Iphone from '../assets/images/iphone-14.jpg';
 import HoldingIphone from '../assets/images/iphone-hand.png';
 
 const Jumbotron = () => {
+
+  const handlerLearnMore = () => {
+   const element= document.querySelector('.sound-section');
+    window.scrollTo({ 
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: 'smooth'
+    })
+  }
   return (
     <div className='jumbotron-section wrapper'>
           <h2 className='title'>New</h2>
@@ -14,7 +23,7 @@ const Jumbotron = () => {
                   <button className='button'>Buy</button>
               </li>
               <li>
-                  <a className='link'>Learn ore</a>
+                  <a className='link' onClick={handlerLearnMore}>Learn more</a>
               </li>
           </ul>
           <img className='iphone-img' src={HoldingIphone} alt="iPhone" />
